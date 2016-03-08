890f0809bd1696b6b390798ad21129eb2a86307f
Justin Willis <jgw9617@gmail.com>
2016-03-08 00:57:10 -0600

    changes

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,12 +1,17 @@
 import {Page} from 'ionic-angular';
 import {Http} from "angular2/http";
+import {OnInit} from "angular2/core";
 import 'rxjs/add/operator/map';
 
 
 @Page({
     templateUrl: 'build/pages/page1/page1.html',
 })
-export class Page1 {
+export class Page1 implements OnInit {
+    
+    ngOnInit() {
+        this._snapshot();
+    }
     
     public picSource: string;
     public loading: boolean;
@@ -18,10 +23,11 @@ export class Page1 {
         this.loading = false;
     }
 
-    snapshot() {
+    private _snapshot() {
         this.loading = true;
         navigator.geolocation.getCurrentPosition((position) => {
             this.picSource = `https://maps.googleapis.com/maps/api/streetview?size=1000x1000&location=${position.coords.latitude},${position.coords.longitude}&fov=90&heading=235&pitch=10&key=AIzaSyCRLbd9d9sOTljJc3R_M7dMg23WXqhMw8M`;
+            document.querySelector("#mainImage").classList.add("show");
             this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyCRLbd9d9sOTljJc3R_M7dMg23WXqhMw8M`)
             .map(res => res.json())
             .subscribe(data => {
