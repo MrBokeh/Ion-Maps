d50b52a9daa4fdd60b9afd15d18cc082c563b880
Justin Willis <jgw9617@gmail.com>
2016-03-10 01:11:00 -0600

    big update

--- app/pages/page3/page3.ts
+++ app/pages/page3/page3.ts
@@ -11,11 +11,14 @@ declare var google;
 export class Page3 implements OnInit {
 
     public startPosition: string;
+    public endPosition: string;
+    public navigating: boolean;
     directionsService: any;
     public directions: string[];
-    
+
     ngOnInit() {
         this._setCurrent();
+        this.navigating = false;
     }
 
     constructor(public http: Http, public nav: NavController) {
@@ -24,7 +27,7 @@ export class Page3 implements OnInit {
         this.nav = nav;
     }
 
-   private _setCurrent() {
+    private _setCurrent() {
         navigator.geolocation.getCurrentPosition((position) => {
             this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyCRLbd9d9sOTljJc3R_M7dMg23WXqhMw8M`)
                 .map(res => res.json())
@@ -65,6 +68,9 @@ export class Page3 implements OnInit {
                         map: map,
                         directions: response
                     });
+                    
+                    this.navigating = true;
+                    
                 }
                 else {
                     let alert = Alert.create({
@@ -75,8 +81,62 @@ export class Page3 implements OnInit {
                     this.nav.present(alert);
                 }
 
-                
+
             }
         );
+    }
+    
+    done() {
+        this._setCurrent();
+        this.navigating = false;
+        this.directions = null;
+    }
+
+    startSearch() {
+        let prompt = Alert.create({
+            title: 'Search',
+            message: "Search for places nearby",
+            inputs: [
+                {
+                    name: 'Search',
+                    placeholder: 'Search Term'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Search',
+                    handler: data => {
+                        console.log(data);
+                        navigator.geolocation.getCurrentPosition((position) => {
+                            let map = new google.maps.Map(document.getElementById('map'), {
+                                center: { lat: position.coords.latitude, lng: position.coords.longitude },
+                                zoom: 8
+                            });
+                            
+                            let request = {
+                                location: { lat: position.coords.latitude, lng: position.coords.longitude },
+                                radius: "400",
+                                query: data.Search
+                            }
+                            
+                            let service = new google.maps.places.PlacesService(map);
+                            service.textSearch(request, (results, status) => {
+                                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                                    console.log(results);
+                                    this.endPosition = results[0].formatted_address;
+                                }
+                            });
+                        });
+                    }
+                }
+            ]
+        });
+        this.nav.present(prompt);
     }
 }
